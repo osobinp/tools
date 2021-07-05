c2e560ea4f615cb1e69bdaf04af92488a519b19c
Pawel Osobinski <pawel.osobinski@accenture.com>
2021-07-05 14:57:51 +0200

    Update for req

--- virtualenv.sh
+++ virtualenv.sh
@@ -5,10 +5,8 @@ ENV_NAME=env
 BASE_PATH=`basename $0`
 
 if [ "echo $0 | cut -c1" = "/" ]; then
-
  TOOLS_PATH=`dirname $0`
 else
-
  TOOLS_PATH=`pwd`/`echo $0 | sed -e s/$BASE_PATH//`
 fi
 
@@ -17,12 +15,18 @@ cp $TOOLS_PATH/.vimrc $HOME
 cp -R $TOOLS_PATH/.atom $HOME
 
 virtualenv $ENV_NAME
-source $PWD/$ENV_NAME/bin/activate
-python -m pip install --upgrade pip
-pip install -r $TOOLS_PATH/requirements.txt
 
-ansible-playbook $TOOLS_PATH/hashicorp.yml
+if [ $? -eq 0 ]; then
+    source $PWD/$ENV_NAME/bin/activate
+    python -m pip install --upgrade pip
+    pip install -r $TOOLS_PATH/requirements.txt
 
-echo "-----------------"
-echo "Run: source $PWD/$ENV_NAME/bin/activate"
-echo "-----------------" 
+    ansible-playbook $TOOLS_PATH/hashicorp.yml 
+
+    echo "-----------------"
+    echo "Run: source $PWD/$ENV_NAME/bin/activate"
+    echo "-----------------"
+else
+    echo "Install python-pip && pip install virtualenv"
+fi
+ 
