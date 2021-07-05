3bb92e11e15c4a0c4ac433bd35da0acb7261d8d6
Pawel Osobinski <pawel.osobinski@accenture.com>
2021-07-05 14:40:14 +0200

    Added this and that

---
+++ virtualenv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ENV_NAME=env
+
+BASE_PATH=`basename $0`
+
+if [ "echo $0 | cut -c1" = "/" ]; then
+
+ TOOLS_PATH=`dirname $0`
+else
+
+ TOOLS_PATH=`pwd`/`echo $0 | sed -e s/$BASE_PATH//`
+fi
+
+cp $TOOLS_PATH/.bashrc $HOME
+cp $TOOLS_PATH/.vimrc $HOME
+cp -R $TOOLS_PATH/.atom $HOME
+
+virtualenv $ENV_NAME
+source $PWD/$ENV_NAME/bin/activate
+python -m pip install --upgrade pip
+pip install -r $TOOLS_PATH/requirements.txt
+
+ansible-playbook $TOOLS_PATH/hashicorp.yml
+
+echo "-----------------"
+echo "Run: source $PWD/$ENV_NAME/bin/activate"
+echo "-----------------" 
